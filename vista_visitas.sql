bcd3355f76331c2058dfcccfd60301aba7be23ff
osrcbox <osrcbox@gmail.com>
2014-06-13 10:15:38 -0500

    Create vista_visitas.sql

---
+++ vista_visitas.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS `vista_visitas`;
+CREATE VIEW `vista_visitas` AS
+    select 
+		count(v.id) as total, 
+		count(case when v.fecha = date(now()) then 0 else null end) as hoy,
+		(select count(p.id) from contador_visitas_pagina p) as pagina
+	from contador_visitas v
