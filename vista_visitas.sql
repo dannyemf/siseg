c5101a325d19c0360b05736c96c63ded0accccea
osrcbox <osrcbox@gmail.com>
2014-06-13 10:20:57 -0500

    Update vista_visitas.sql

--- vista_visitas.sql
+++ vista_visitas.sql
@@ -3,5 +3,5 @@ CREATE VIEW `vista_visitas` AS
     select 
 		count(v.id) as total, 
 		count(case when v.fecha = date(now()) then 0 else null end) as hoy,
-		(select count(p.id) from contador_visitas_pagina p) as pagina
+		(select count(p.id) from contador_visitas_pagina p) as paginas
 	from contador_visitas v
