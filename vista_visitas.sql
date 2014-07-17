9d356ddb2d81ad395afb1454a6e6dfa5ecc46edd
dannyemf <dannyemf@gmail.com>
2014-07-17 12:14:21 -0500

    Update vista_visitas.sql

--- vista_visitas.sql
+++ vista_visitas.sql
@@ -1,7 +1,9 @@
 DROP VIEW IF EXISTS `vista_visitas`;
 CREATE VIEW `vista_visitas` AS
-    select 
-		count(v.id) as total, 
-		count(case when v.fecha = date(now()) then 0 else null end) as hoy,
-		(select count(p.id) from contador_visitas_pagina p) as paginas
-	from contador_visitas v
+select
+	count(v.id) as total,
+	count(case when v.fecha = date(now()) then 0 else null end) as hoy,
+	(select count(*) from contador_visitas where week(now(),1) = week(fecha,1)) as esta_semana,
+	(select count(*) from contador_visitas where month(now()) = month(fecha)) as este_mes,
+	(select count(p.id) from contador_visitas_pagina p) as paginas
+from contador_visitas v
