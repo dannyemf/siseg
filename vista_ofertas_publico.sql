2a98a46d494d19e999d25b0e6c56784ffd4e0902
osrcbox <osrcbox@gmail.com>
2014-07-31 17:25:16 -0500

    Update vista_ofertas_publico.sql

--- vista_ofertas_publico.sql
+++ vista_ofertas_publico.sql
@@ -1,7 +1,7 @@
 DROP VIEW IF EXISTS `vista_ofertas_publico`;
 CREATE VIEW `vista_ofertas_publico` AS
 SELECT
-o.id ide_oferta, o.puesto, o.salario, o.descripcion, o.postulacion_desde, o.postulacion_hasta, o.fecha_entrevista, o.ubicacion, o.estado, o.sexo, o.edad,
+o.id ide_oferta, o.puesto, o.salario, o.descripcion, o.postulacion_desde, o.postulacion_hasta, o.fecha_entrevista, o.ubicacion, o.estado, o.sexo, o.edad, o.edad_desde, o.edad_hasta,
 e.ide_empleador, e.nombre_empresa, e.logo,
 (case when date(now()) >= o.postulacion_desde and date(now()) <= o.postulacion_hasta then 1 else 0 end) as fecha_aplicable
 FROM
